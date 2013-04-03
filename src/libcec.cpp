a480126437b9dd890ab2bfd14ab019080daa8f67
Phill <phillip.camp+git@gmail.com>
2013-04-03 17:01:10 +0100

    Re assert disbale combo key to make stop work

--- src/libcec.cpp
+++ src/libcec.cpp
@@ -118,6 +118,9 @@ ICECAdapter * Cec::CecInit(const char * name, CecCallback * callback) {
 	callbacks.CBCecConfigurationChanged = &::cecConfigurationChanged;
 	config.callbackParam                = callback;
 	config.callbacks                    = &callbacks;
+	
+	// Disable combo keys for making STOP work
+	config.comboKey                     = CEC_USER_CONTROL_CODE_UNKNOWN; 
 
 	// LibCecInitialise is noisy, so we redirect cout to nowhere
 	RedirectStreamBuffer redirect(cout, 0);
