745f6c3e1c89245f7737878ae6a2073643198308
Mario <mario_lange@gmx.net>
2015-08-07 12:56:11 +0200

    depencies for debian testing
    added some needed depencies for deb package

--- build_deb.sh
+++ build_deb.sh
@@ -12,7 +12,7 @@ checkinstall \
 		--nodoc \
 		--pkgversion="0.9" \
 		--pkggroup="system" \
-		--requires='' \
+		--requires="libboost-program-options1.55.0, liblog4cplus-1.0-4, libcec3, liblockdev1, libboost-system1.55.0, libboost-thread1.55.0, libstdc++6" \
 		--pkgname="libcec-daemon" \
 		--maintainer='KingBonecrusher' \
 		--provides='libcec-daemon' \
